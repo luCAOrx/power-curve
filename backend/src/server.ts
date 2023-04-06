b8b5de5faf2670bdc102139239884d4a88867208
luCAOrx <lucaorx@protonmail.com>
2023-04-05 21:18:57 -0300

    refactor: Moved core functionality to app.ts file, and added port on which server will be available

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,3 @@
-import express from 'express';
+import {app} from './app'
 
-import cors from 'cors';
-
-import './database/connection';
-
-import 'express-async-errors';
-
-import errorHandler from './errors/handler';
-
-import routes from './routes';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-app.use(errorHandler);
-
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
