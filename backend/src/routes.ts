d5cfe509a7e1a46ae1e41c85f79890695d46b5d2
luCAOrx <lucaorx@protonmail.com>
2023-04-05 21:14:55 -0300

    refactor: Changed name of imports, routes and controllers

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,23 +1,23 @@
-import { Router } from 'express';
+import {Router} from 'express';
 
 import multer from 'multer';
 
 import uploadConfig from './config/upload';
 
-import PowrtCurvesValitdation from './validations/PowerCurvesValidation';
+import PowerCurveValidation from './validations/PowerCurvesValidation';
 
-import PowerCurvesController from './controllers/PowerCurvesController';
+import {PowerCurveController} from './controllers/PowerCurvesController';
 
 const routes = Router();
 
 const upload = multer(uploadConfig);
 
-routes.get('/power_curves', PowerCurvesController.index);
+routes.get('/power_curve/list-all', new PowerCurveController().index);
 
-routes.post('/power_curves', 
-  PowrtCurvesValitdation.validate,
-  upload.array('files', 1), 
-  PowerCurvesController.create,
+routes.post('/power_curve/create',
+  upload.single('file'),
+  PowerCurveValidation.validate,
+  new PowerCurveController().create,
 );
 
 export default routes;
