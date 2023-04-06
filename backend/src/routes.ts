56786a6966f979678cf612a66fd3bd87ad9a7801
luCAOrx <lucaorx@protonmail.com>
2023-04-05 21:39:13 -0300

    refactor: Removed unnecessary import and added semantic class methods

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,18 +6,19 @@ import uploadConfig from './config/upload';
 
 import PowerCurveValidation from './validations/PowerCurvesValidation';
 
-import {PowerCurveController} from './controllers/PowerCurvesController';
+import {CreatePowerCurveController} from './controllers/createPowerCurve/createPowerCurveController';
+import {ListAllPowerCurveController} from './controllers/listAllPowerCurve/listAllPowerCurveController';
 
 const routes = Router();
 
 const upload = multer(uploadConfig);
 
-routes.get('/power_curve/list-all', new PowerCurveController().index);
+routes.get('/power_curve/list-all', new ListAllPowerCurveController().handle);
 
 routes.post('/power_curve/create',
   upload.single('file'),
   PowerCurveValidation.validate,
-  new PowerCurveController().create,
+  new CreatePowerCurveController().handle,
 );
 
 export default routes;
