d269218136d736ffab79c68e04f2d0949e9c8021
luCAOrx <lucaorx@protonmail.com>
2023-05-09 20:10:27 -0300

    feat: Added repository to follow Liskov Substitution Principle (LSP) and Dependency Inversion Principle (DIP)

---
+++ backend/src/domain/repositories/power-curve-repository.ts
@@ -0,0 +1,7 @@
+import {PowerCurve} from "../entities/power-curve";
+
+export interface PowerCurveRepository {
+  exists: (name: string) => Promise<boolean>
+  create: (powerCurve: PowerCurve) => Promise<PowerCurve>
+  findMany: (page: number, takePage: number) => Promise<PowerCurve[]>
+}
