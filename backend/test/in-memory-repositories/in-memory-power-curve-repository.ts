10d0ce1b11a5cfe0558945551968b1cb5addf7ed
luCAOrx <lucaorx@protonmail.com>
2023-05-09 22:43:21 -0300

    test: Added in-memory database

---
+++ backend/test/in-memory-repositories/in-memory-power-curve-repository.ts
@@ -0,0 +1,22 @@
+import {PowerCurve} from "@domain/entities/power-curve";
+import {PowerCurveRepository} from "@domain/repositories/power-curve-repository";
+
+export class InMemoryPowerCurveRepository implements PowerCurveRepository {
+  public powerCurves: PowerCurve[] = []
+
+  async exists(name: string): Promise<boolean> {
+    return this.powerCurves.some(powerCurve => powerCurve.props.name === name)
+  }
+
+  async create(powerCurve: PowerCurve): Promise<PowerCurve> {
+    this.powerCurves.push(powerCurve)
+
+    return powerCurve
+  }
+
+  async findMany(page: number, takePage: number): Promise<PowerCurve[]> {
+    const powerCurvesOrPowerCurve = this.powerCurves.map(powerCurves => powerCurves)
+
+    return powerCurvesOrPowerCurve.slice((page - 1) * takePage, page * takePage)
+  }
+}
