2c5c2eceddf4b58ddacacbe440b165bba6ee262e
luCAOrx <lucaorx@protonmail.com>
2023-05-09 22:36:12 -0300

    test: Added file that makes the power curve following the factory pattern

---
+++ backend/test/factories/make-power-curve-factory.ts
@@ -0,0 +1,40 @@
+import {resolve} from 'node:path'
+import request from 'supertest'
+import {app} from '@infra/app'
+import {CreatePowerCurveUseCase} from '@domain/use-case/create-power-curve-use-case'
+import {PowerCurve} from '@domain/entities/power-curve'
+import {InMemoryPowerCurveRepository} from '@test/in-memory-repositories/in-memory-power-curve-repository'
+
+interface CreatePowerCurveDTO {
+  name: string
+  file: string
+}
+
+type Override = Partial<CreatePowerCurveDTO>;
+
+export class MakePowerCurve {
+  async toDomain({
+    inMemoryPowerCurveRepository,
+    override
+  }: {
+    inMemoryPowerCurveRepository: InMemoryPowerCurveRepository,
+    override?: Override
+  }): Promise<PowerCurve> {
+    const createPowerCurveUseCase = await new CreatePowerCurveUseCase(inMemoryPowerCurveRepository).execute({
+      name: 'example-power-curve-name',
+      file: 'example-power-curve-file.csv',
+      ...override
+    })
+
+    return createPowerCurveUseCase
+  }
+
+  async toHTTP({file, name}: {name?: string, file?: string}): Promise<request.Test> {
+    const filePath = resolve(__dirname, '..', 'files/example-Abr-2017-curva-potencia-windbox.csv')
+
+    return await request(app)
+      .post('/power_curve/create')
+      .field('name', name ?? 'curva-de-potência-04-2017')
+      .attach('file', file ?? filePath)
+  }
+}
