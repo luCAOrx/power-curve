3ffb64e9f94b383926f41a712be12431405589fe
luCAOrx <lucaorx@protonmail.com>
2023-05-09 22:24:41 -0300

    feat: Added prisma repository following Liskov Substitution Principle (LSP) and Dependency Inversion Principle (DIP)

---
+++ backend/src/infra/repositories/prisma/prisma-power-curve-repository.ts
@@ -0,0 +1,47 @@
+import {PowerCurve} from "@domain/entities/power-curve";
+import {PowerCurveRepository} from "@domain/repositories/power-curve-repository";
+import {PowerCurveMapper} from "@infra/mappers/power-curve-mapper";
+import {PrismaClient} from "@prisma/client";
+
+const prismaClient = new PrismaClient()
+
+export class PrismaPowerCurveRepository implements PowerCurveRepository {
+  async exists(name: string): Promise<boolean> {
+    const powerCurveExists = await prismaClient.powerCurve.findUnique({
+      where: {name}
+    })
+
+
+    return !!powerCurveExists
+  }
+
+  async create(powerCurve: PowerCurve): Promise<PowerCurve> {
+    const {
+      id,
+      name,
+      file,
+      created_at
+    } = PowerCurveMapper.toPersistence(powerCurve)
+
+    const createdPowerCurve = await prismaClient.powerCurve.create({
+      data: {
+        id,
+        name,
+        file,
+        created_at
+      }
+    })
+
+    return PowerCurveMapper.toDomain(createdPowerCurve)
+  }
+
+  async findMany(page: number, takePage: number): Promise<PowerCurve[]> {
+    const powerCurves = await prismaClient.powerCurve.findMany({
+      orderBy: {id: 'desc'},
+      take: takePage,
+      skip: (page - 1) * takePage
+    })
+
+    return powerCurves.map(powerCurve => PowerCurveMapper.toDomain(powerCurve))
+  }
+}
