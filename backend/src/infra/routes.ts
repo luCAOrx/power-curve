1733f62413800b79105365110941a67d5727f901
luCAOrx <lucaorx@protonmail.com>
2023-05-16 20:32:27 -0300

    docs: Added route to documentation, and added interfaces to be used in the documentation route

--- backend/src/infra/routes.ts
+++ backend/src/infra/routes.ts
@@ -1,4 +1,4 @@
-import {Router} from 'express';
+import {Request, Response, Router} from 'express';
 
 import multer from 'multer';
 
@@ -7,9 +7,23 @@ import uploadConfig from './config/upload';
 import {CreatePowerCurveController} from './controllers/create-power-curve/create-power-curve-controller';
 import {ListPowerCurvesController} from './controllers/list-power-curves/list-power-curve-controller';
 
+import {generateHTML, serve} from 'swagger-ui-express'
+
+import {resolve} from 'node:path';
+import {readFileSync} from 'node:fs';
+
 const routes = Router();
 
 const upload = multer(uploadConfig);
+
+routes.use('/api-docs', serve)
+routes.get('/api-docs', (request: Request, response: Response) => {
+  return response.send(generateHTML(
+    JSON.parse(
+      readFileSync(resolve(__dirname, '..', '..', 'swagger.json')).toString()
+    )
+  ))
+})
 
 routes.get('/power_curve/list', new ListPowerCurvesController().handle);
 
