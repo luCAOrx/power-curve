344713016a93b9c18f122d4d2a0e6446a2ebc917
luCAOrx <lucaorx@protonmail.com>
2023-05-16 18:27:30 -0300

    chore: Encapsulating errors in classes

--- backend/src/domain/entities/power-curve.ts
+++ backend/src/domain/entities/power-curve.ts
@@ -1,4 +1,10 @@
 import {randomUUID} from 'node:crypto';
+import {FileMustBeLessThan200Characters} from './errors/file-must-be-less-than-200-characters';
+import {FileMustBeMoreThan25Characters} from './errors/file-must-be-more-than-25-characters';
+import {FileShouldNotBeEmpty} from './errors/file-should-not-be-empty';
+import {NameMustBeLessThan255Characters} from './errors/name-must-be-less-than-255-characters';
+import {NameMustBeThan5Characters} from './errors/name-must-be-than-5-characters';
+import {NameShouldNotBeEmpty} from './errors/name-should-not-be-empty';
 
 type PowerCurveProps = {
   name: string
@@ -35,18 +41,18 @@ export class PowerCurve {
   }
 
   static create(props: PowerCurveProps, id?: string): PowerCurve {
-    if (!props.name) throw new Error('The field name should not be empty')
-    if (props.name.length > 255) throw new Error('The field name must be less than 255 characters')
-    if (props.name.length < 5) throw new Error('The field name must be than 5 characters')
+    if (!props.name) throw new NameShouldNotBeEmpty()
+    if (props.name.length > 255) throw new NameMustBeLessThan255Characters()
+    if (props.name.length < 5) throw new NameMustBeThan5Characters()
 
     if (
       !props.file ||
       props.file === 'null' ||
       props.file === 'undefined' ||
       (typeof props.file === 'string' && props.file.trim() === '')
-    ) throw new Error('The field file should not be empty')
-    if (props.file.length > 200) throw new Error('The field file must be less than 200 characters')
-    if (props.file.length < 25) throw new Error('The file field must be more than 25 characters')
+    ) throw new FileShouldNotBeEmpty()
+    if (props.file.length > 200) throw new FileMustBeLessThan200Characters()
+    if (props.file.length < 25) throw new FileMustBeMoreThan25Characters()
 
     return new PowerCurve(props, id)
   }
